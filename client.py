8c1d409f717ec2a3d1acf4f7b03d17c85d33e395
Braden <bradennnn@gmail.com>
2019-04-15 12:32:13 -0400

    Updated

--- client.py
+++ client.py
@@ -38,3 +38,19 @@ class R710_Lgsm(SSHClient):
 # return 3tuple of std.
         return stdin, stdout, stderr
     pass
+
+
+def r710_ssh(cmd):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.get_host_keys().add('games.brefend.com', 'ssh-rsa', SSH_KEY)
+    #Connection
+    client.connect(IPADDR, username="kf2server", password="bonesniff")
+    stdin, stdout, stderr = client.exec_command(f"{cmd}")
+    stdarr = [stdin, stdout, stderr]
+    for line in stdout:
+        print('... ' + line.strip('\n'))
+    client.close()
+    return stdarr
+    # make a ssh client instance
+    # return output of a command given in arguments perhaps.
--- game_servers.py
+++ game_servers.py
@@ -1,14 +1,12 @@
-from client import client
+from client import SSH_KEY
+from client import r710_ssh
+import paramiko
+
 #from server import GameServer
 
 
 NAME, IPADDR, LGSM_NAME = "Brazen Floor 2", "192.168.0.155", "kf2server"
 
-def r710_ssh():
-    client = paramiko.SSHClient()
-    stdin, stdout, stderr = client.exec_command(f"./{self.lgsm_name} start")
-    # make a ssh client instance
-    # return output of a command given in arguments perhaps.
 class GameServer: #The base class.
     # Server object
     # Default address is always r710 for game servers
@@ -19,10 +17,6 @@ class GameServer: #The base class.
         self.name = name
         self.lgsm_name = lgsm_name
 
-    def isrunning(self, name):
-
-        pass
-
     def start(self): #default start for any server
         client.connect(self.ipaddr, username=self.lgsm_name, password="bonesniff")
         print("kf2server connected")
@@ -31,12 +25,12 @@ class GameServer: #The base class.
             print('... ' + line.strip('\n'))
         client.close()
 
-     def stop(self):
-        client.connect(self.ipaddr, username=self.lgsm_name, password="bonesniff")
-        stdin, stdout, stderr = client.exec_command(f"./{self.lgsm_name} stop")
-        for line in stdout:
-            print('... ' + line.strip('\n'))
-        client.close()
+     # def stop(self):
+     #    client.connect(self.ipaddr, username=self.lgsm_name, password="bonesniff")
+     #    stdin, stdout, stderr = client.exec_command(f"./{self.lgsm_name} stop")
+     #    for line in stdout:
+     #        print('... ' + line.strip('\n'))
+     #    client.close()
 
 class KillingFloor2_S(GameServer):
 
@@ -45,6 +39,10 @@ class KillingFloor2_S(GameServer):
 
     def __init__(self, name):
         self.name = name
+
+    def start(self):
+        stdarr = r710_ssh("./kf2server start")
+        stdout = stdarr[1]
 
     #define a custom start function for different types of kf2 servers.
 
