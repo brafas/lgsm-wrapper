4fe40e7c28569cba320e6d5963dc7745ec0dd9f5
Braden <bradennnn@gmail.com>
2019-04-22 17:21:43 -0400

    Added SFTP for r710 in client.

--- client.py
+++ client.py
@@ -1,33 +1,16 @@
 import base64
 import paramiko
+import traceback
+import os
+
 from paramiko import SSHClient
 
 R710_IP = "192.168.0.155"
-
-SSH_KEY = paramiko.RSAKey(data=base64.b64decode(
-    b'AAAAB3NzaC1yc2EAAAABJQAAAQEArhpylh4AEOrcg5RDHq8zHKZbxeWGJRX9GrlIjYFPyoME0DiFOA4lh/907GsRoB \
-    /jp6dleGAl/I+74oQv+bgeGlrmnK+e8TpmjzPDSNCkFxYH5vFI38LTmjRcgJXd8kvDSrA7goPBk1bjLJ6o6wAWqfxQGTBktG6C2aPh/a \
-    AGeVRNY6JNUut1fRhij5C8zXotndPvivIER0OidBmyTasRsFualAhz2UaF9LnWwDLKMhQJ2nhjdhy38bF2NNNdJdWHWetDhKRbiep1nN \
-    n24Oh1J6iKs2P86AZq/+csO4b7EmqH8eRIwJ9iLoY+lZCjOZ82K7Esp4sxh5sSblWoxOpAfw=='))
-
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.get_host_keys().add('games.brefend.com', 'ssh-rsa', SSH_KEY)
-#client.connect(LOCAL_IP, username='kf2server', password='bonesniff')
-
-# class R710_Ssh:
-#     def __init__(self, lgsm_name):
-#         self.lgsm_name = lgsm_name
-#
-#     def connect(self):
-#         client = paramiko.SSHClient()
-#         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#         client,.get_host_keys().add('games.brefend.com', 'ssh-rsa', SSH_KEY)
-#         client.connect(LOCAL_IP, username=self.lgsm_name, password='bonesniff')
-#
-#     def disconnect(self):
-#         client.close()
-
+#Public key
+SSH_KEY = paramiko.RSAKey(data=base64.b64decode(b'AAAAB3NzaC1yc2EAAAABJQAAAQEArhpylh4AEOrcg5RDHq8zHKZbxeWGJRX9GrlIjYFPyoME0DiFOA4lh/907GsRoB \
+        /jp6dleGAl/I+74oQv+bgeGlrmnK+e8TpmjzPDSNCkFxYH5vFI38LTmjRcgJXd8kvDSrA7goPBk1bjLJ6o6wAWqfxQGTBktG6C2aPh/a \
+        AGeVRNY6JNUut1fRhij5C8zXotndPvivIER0OidBmyTasRsFualAhz2UaF9LnWwDLKMhQJ2nhjdhy38bF2NNNdJdWHWetDhKRbiep1nN \
+        n24Oh1J6iKs2P86AZq/+csO4b7EmqH8eRIwJ9iLoY+lZCjOZ82K7Esp4sxh5sSblWoxOpAfw=='))
 
 def r710_ssh(cmd):
     #send a cmd, return stdout. or stderr.. Close connection
@@ -43,3 +26,32 @@ def r710_ssh(cmd):
     client.close()
     # make a ssh client instance
     # return output of a command given in arguments perhaps.
+
+def r710_sftp():
+    hostname = "192.168.0.155" #r710 server.
+    username = "kf2server"
+    password = "bonesniff"
+    #load host keys
+    host_keys = paramiko.util.load_host_keys(
+        os.path.expanduser("~/.ssh/known_hosts")
+    )
+    if hostname in host_keys:
+        hostkeytype = host_keys[hostname].keys()[0]
+        hostkey = host_keys[hostname][hostkeytype]
+
+    try:
+        t = paramiko.Transport((hostname, 22))
+        t.connect(hostkey, username, password)
+        sftp = paramiko.SFTPClient.from_transport(t)
+        dirlist = sftp.listdir(".")
+        print(f"Dirlist: {dirlist}")
+        t.close()
+
+    except Exception as e:
+        print("*** Caught exception: %s: %s" % (e.__class__, e))
+        traceback.print_exc()
+        try:
+            t.close()
+        except:
+            pass
+r710_sftp()
